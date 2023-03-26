f88f310130bb20c1b6cca7c093b6d03de0e3716f
Gabriel Valdivia <vgabriel.gm37@gmail.com>
2023-03-26 13:26:54 -0500

    distancia euclidiana todos a uno

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ pygame.init()
 Globals
 '''
 fps = 15
+inf = 999999
 fpsClock = pygame.time.Clock()
 flags = DOUBLEBUF
 DISPLAYSURF = pygame.display.set_mode((1280, 720), flags, 16)
@@ -24,11 +25,15 @@ class node:
     def __init__(self,name,pos):
         self.name = name
         self.pos = pos
+        self.active = True
     def __str__(self):
         return f"{self.name}({self.pos})"
     def drawNode(self, color):
-        n = pygame.draw.circle(DISPLAYSURF,color,self.pos,node_size)
-        return n
+        if self.active:
+            pygame.draw.circle(DISPLAYSURF,color,self.pos,node_size)
+        
+    def deactivate(self):
+        self.active = False
 
 class matAdy:
     def __init__(self):
@@ -41,8 +46,9 @@ class matAdy:
             str.append("|")
         return f"{str}"
     def addEdge(self,node1, node2):
-        self.mat[node1][node2] = 1
-        self.mat[node2][node1] = 1
+        if nodes[node1].active and nodes[node2].active:
+            self.mat[node1][node2] = 1
+            self.mat[node2][node1] = 1
     def drawEdges(self, color):
         for i in range(num_nodes):
             for j in range(num_nodes):
@@ -52,27 +58,29 @@ class matAdy:
                 
 def generateNodes():
     h,v = makelimits()
-    nodes = []
+    nodes = list(range(0,num_nodes))
     for i in range(0,dim[0]):
         for j in range(0,dim[1]):
-            nodes.append(node( (dim[0]*i) + j, (i*h + node_size,j*v + node_size) ))
+            nodes[(dim[0]*i)+j] = node(
+                (dim[0]*i)+j, (i*h + node_size,j*v + node_size) )
     return nodes
 
 def generateEdges():
     for i in range(0,num_nodes):
-        if((i+1) % dim[1] > 0):
-            if(i+1 + dim[1] <= num_nodes):
-                mAdy.addEdge(i,i+1)
-                mAdy.addEdge(i,i+dim[1])
-                mAdy.addEdge(i,i+dim[1]+1)
+        if nodes[i].active:
+            if((i+1) % dim[1] > 0):
+                if(i+1 + dim[1] <= num_nodes):
+                    mAdy.addEdge(i,i+1)
+                    mAdy.addEdge(i,i+dim[1])
+                    mAdy.addEdge(i,i+dim[1]+1)
+                else:
+                    mAdy.addEdge(i,i+1)
             else:
-                mAdy.addEdge(i,i+1)
-        else:
-            if(i+1 + dim[1] <= num_nodes):
-                mAdy.addEdge(i,i+dim[1])
-        if(i % dim[1] > 0):
-            if(i+1 + dim[1] <= num_nodes):
-                mAdy.addEdge(i,i+dim[1]-1)
+                if(i+1 + dim[1] <= num_nodes):
+                    mAdy.addEdge(i,i+dim[1])
+            if(i % dim[1] > 0):
+                if(i+1 + dim[1] <= num_nodes):
+                    mAdy.addEdge(i,i+dim[1]-1)
 
 def drawNodes(nodes, color):
     for i in nodes:
@@ -98,15 +106,26 @@ def printNodes():
     for i in nodes:
         print(i)
 
-
-    
+def calculateDistances(fin): #1 a todos
+    if not nodes[fin].active:
+        print("Error. nodo destino no existente.")
+        return []
+    distances = [inf for _ in range(num_nodes)]
+    for i in range(0,num_nodes):
+        if nodes[i].active:
+            distances[i] = distance.euclidean(nodes[i].pos,nodes[fin].pos)
+        else:
+            distances[i] = inf
+    return distances
     
 mAdy = matAdy()        
 nodes = generateNodes()
+nodes[4].deactivate()
 generateEdges()
 drawNodes(nodes,[255,0,0])
-print(mAdy)
 mAdy.drawEdges([255,0,0])
+dis = calculateDistances(0)
+print("dis: ",dis)
 
 #printNodes()
 
